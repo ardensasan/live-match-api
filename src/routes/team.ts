8c4eebdbfc5d83857362194c6f3ef2ac724f33a8
Arden Johannes Sasan <ajsasan@dnamicro.com>
2022-01-03 14:14:10 +0800

    used req.params.ugc_id as logo id

--- src/routes/team.ts
+++ src/routes/team.ts
@@ -8,7 +8,7 @@ route.get("/team/", (req: Request, res: Response) => {});
 route.get("/team/logo/:ugc_id", async (req: Request, res: Response) => {
   try {
     const { data } = await axios.get(
-      `https://api.steampowered.com/ISteamRemoteStorage/GetUGCFileDetails/v1/?key=5B94DAD2545FCAA73447EEF18ED8432A&appid=570&ugcid=975479762873971573`
+      `https://api.steampowered.com/ISteamRemoteStorage/GetUGCFileDetails/v1/?key=5B94DAD2545FCAA73447EEF18ED8432A&appid=570&ugcid=${req.params.ugc_id}`
     );
     return res.send(data);
   } catch (error) {
